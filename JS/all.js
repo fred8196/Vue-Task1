65561660ebb06b4122e09333b903e14912155b41
Fred Chang <fred8196@gmail.com>
2021-05-10 23:14:26 +0800

    Initial commit

---
+++ JS/all.js
@@ -0,0 +1,73 @@
+let productData = [];
+const productTitle = document.querySelector('#title');
+const productOriginPrice = document.querySelector('#origin_price');
+const productPrice = document.querySelector('#price');
+const addProduct = document.querySelector('#addProduct')
+const productId = Math.floor(Date.now());
+
+//新增產品
+addProduct.addEventListener('click', () => {
+    if (productTitle.value.trim() == '' || productOriginPrice.value == '' || productPrice.value == '') {
+        alert('請填寫完整產品資料');
+        return;
+    }
+    productData.push({
+        title: productTitle.value,
+        originPrice: parseInt(productOriginPrice.value) || 0,
+        price: parseInt(productPrice.value) || 0,
+        id: productId,
+        status: false
+    });
+    console.log(productData);
+    renderProductList();
+})
+
+//渲染染品列表
+const productList = document.querySelector('#productList');
+const productCount = document.querySelector('#productCount');
+function renderProductList() {
+    let str = '';
+    productData.forEach((item, index) => {
+        str += `<tr>
+        <td>${item.title}</td>
+        <td width="120">
+            $ ${item.originPrice}
+        </td>
+        <td width="120">
+            $ ${item.price}
+        </td>
+        <td width="100">
+            <div class="form-check form-switch">
+                <input class="form-check-input" type="checkbox" id="is_enabled" ${item.status
+                ? 'checked' : ''} data-action="complete" data-id="${index}">
+                <label class="form-check-label" for="is_enabled">${item.status ? '啟用' : '未啟用'}</label>
+            </div>
+        </td>
+        <td width="120">
+            <button type="button" class="btn btn-sm btn-danger move" data-action="remove"
+                data-id="${index}"> 刪除 </button>
+        </td>
+    </tr>`
+    })
+    productList.innerHTML = str;
+    productCount.textContent = productData.length;
+}
+
+//刪除產品.變更狀態
+productList.addEventListener('click', (e) => {
+    productIndex = e.target.dataset.id;
+    if (e.target.dataset.action == 'remove') {
+        productData.splice(productIndex, 1);
+    } else if (e.target.dataset.action == 'complete') {
+        productData[productIndex].status = !productData[productIndex].status;
+    }
+    renderProductList();
+})
+
+//刪除全部產品
+const clearAll = document.querySelector('#clearAll');
+clearAll.addEventListener('click', () => {
+    productData.length = 0;
+    renderProductList();
+})
+
