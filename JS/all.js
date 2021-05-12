02b03e95a6d507b057bbb2634177cdb0d63ad272
Fred Chang <fred8196@gmail.com>
2021-05-12 21:37:23 +0800

    Update all.js

--- JS/all.js
+++ JS/all.js
@@ -3,10 +3,11 @@ const productTitle = document.querySelector('#title');
 const productOriginPrice = document.querySelector('#origin_price');
 const productPrice = document.querySelector('#price');
 const addProduct = document.querySelector('#addProduct')
-const productId = Math.floor(Date.now());
+const addProductForm = document.querySelector('.addProductForm');
 
 //新增產品
 addProduct.addEventListener('click', () => {
+    const productId = Math.floor(Date.now());
     if (productTitle.value.trim() == '' || productOriginPrice.value == '' || productPrice.value == '') {
         alert('請填寫完整產品資料');
         return;
@@ -20,6 +21,7 @@ addProduct.addEventListener('click', () => {
     });
     console.log(productData);
     renderProductList();
+    addProductForm.reset();
 })
 
 //渲染染品列表
@@ -38,9 +40,9 @@ function renderProductList() {
         </td>
         <td width="100">
             <div class="form-check form-switch">
-                <input class="form-check-input" type="checkbox" id="is_enabled" ${item.status
+                <input class="form-check-input" type="checkbox" id=${item.id} ${item.status
                 ? 'checked' : ''} data-action="complete" data-id="${index}">
-                <label class="form-check-label" for="is_enabled">${item.status ? '啟用' : '未啟用'}</label>
+                <label class="form-check-label" for=${item.id}>${item.status ? '啟用' : '未啟用'}</label>
             </div>
         </td>
         <td width="120">
@@ -58,10 +60,11 @@ productList.addEventListener('click', (e) => {
     productIndex = e.target.dataset.id;
     if (e.target.dataset.action == 'remove') {
         productData.splice(productIndex, 1);
+        renderProductList();
     } else if (e.target.dataset.action == 'complete') {
         productData[productIndex].status = !productData[productIndex].status;
+        renderProductList();
     }
-    renderProductList();
 })
 
 //刪除全部產品
